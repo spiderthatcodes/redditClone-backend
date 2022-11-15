bd7fc86b503aaef778504609762c99f9c8461f02
spiderthatcodes <75698869+jdickerson-banyan@users.noreply.github.com>
2022-11-15 10:00:59 -0700

    repository files

---
+++ src/main/java/com/example/redditclone/repository/CommentRepository.java
@@ -0,0 +1,14 @@
+package com.example.redditclone.repository;
+
+import com.example.redditclone.model.Comment;
+import com.example.redditclone.model.Post;
+import com.example.redditclone.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPost(Post post);
+
+    List<Comment> findAllByUser(User user);
+}
---
+++ src/main/java/com/example/redditclone/repository/VoteRepository.java
@@ -0,0 +1,12 @@
+package com.example.redditclone.repository;
+
+import com.example.redditclone.model.Post;
+import com.example.redditclone.model.User;
+import com.example.redditclone.model.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+    Optional<Vote> findTopByPostAndUserOrderByVoteId(Post post, User currentUser);
+}
