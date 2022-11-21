ec4c660cf819a3d9cd064fbb7d3aba2b18cdd6af
spiderthatcodes <75698869+jdickerson-banyan@users.noreply.github.com>
2022-11-21 10:10:14 -0700

    generateVerificationToken

--- src/main/java/com/example/redditclone/service/AuthService.java
+++ src/main/java/com/example/redditclone/service/AuthService.java
@@ -2,12 +2,16 @@ package com.example.redditclone.service;
 
 import com.example.redditclone.dto.RegisterRequest;
 import com.example.redditclone.model.User;
+import com.example.redditclone.model.VerificationToken;
 import com.example.redditclone.repository.UserRepository;
+import com.example.redditclone.repository.VerificationTokenRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 import static java.time.Instant.now;
 
@@ -18,6 +22,7 @@ public class AuthService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final VerificationTokenRepository verificationTokenRepository;
 
     @Transactional
     public void signup(RegisterRequest registerRequest) {
@@ -29,6 +34,18 @@ public class AuthService {
         user.setEnabled(false);
 
         userRepository.save(user);
+
+        String token = generateVerificationToken(user);
+    }
+
+    private String generateVerificationToken(User user) {
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+        verificationTokenRepository.save(verificationToken);
+
+        return token;
     }
 
     private String encodePassword(String password) {
